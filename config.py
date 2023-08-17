ed2581f209451240f029085479e51d6ee5b6493c
hdneudneidej8382929292983 <141856645+hdneudneidej8382929292983@users.noreply.github.com>
2023-08-17 17:00:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     API_ID = 19312827
     API_HASH = "84da7f08e87849853b2fa6728e4192a2"
-    TOKEN = "6225367412:AAF_stl_eJUgacMubl8D7C4mFUf9LVxEL_g"  
+    TOKEN = "6225367412:AAFBSNn3rz99TtrgW7AgAVTOwLliQ8GDtJw"    
     START_PIC = "https://telegra.ph/file/e4055f1c03998748f2d55.jpg" 
     CHAT = "5108562302"    
